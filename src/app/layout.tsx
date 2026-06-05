44e6a8696f9bec77d30739075c5b2b4f795225cc
Pedro Henrique <sucessopen@gmail.com>
2026-06-05 01:27:13 +0000

    adicione meu script de utm>
    <script
      src="https://cdn.utmify.com.br/

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,12 @@ export default function RootLayout({
             document.head.appendChild(a);
           `}
         </Script>
+        <Script
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-xcod-sck
+          data-utmify-prevent-subids
+          strategy="afterInteractive"
+        />
       </head>
       <body className="font-body antialiased">{children}</body>
     </html>
